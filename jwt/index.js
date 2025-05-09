0729321a5e7f45a87ce286edb742c932247c3a08
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-09 23:53:28 +0530

    generate jwt token

---
+++ jwt/index.js
@@ -0,0 +1,20 @@
+
+import jwt from "jsonwebtoken";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+const generateToken = function (user_id) {
+    const token= jwt.sign("this demo text", "secrete key");
+
+    console.log(token)
+  };
+
+generateToken()
