66a32c4c7901a0883c2f100f5f016cd3fa7d8eea
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-10 23:40:41 +0530

    node js file system

---
+++ fileSystem/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+
+
+// create empty file
+
+var fs = require('fs');
+
+fs.open('demoEmpty.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
