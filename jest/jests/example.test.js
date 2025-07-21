ad2b136a5cd8749d3c705dd34ec2cf8815ed1466
rupeshpatil <rupeshpatil88927@gmail.com>
2025-07-21 23:57:28 +0530

    add basic test examples

--- jest/jests/example.test.js
+++ jest/jests/example.test.js
@@ -1,5 +1,16 @@
-import { sum } from "../utils/utils.js";
+import { checkTypeSum, fetchData, sum } from "../utils/utils.js";
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 1 to equal 2", () => {
   expect(sum(1, 1)).toBe(2);
 });
+
+test("check type and add numbers", () => {
+  expect(()=>{
+    checkTypeSum(1, 1)
+  }).toThrow();
+});
+
+test("fetch basic data", () => {
+ expect(fetchData()).toBe({name:'abc'});
+});
+
--- jest/utils/utils.js
+++ jest/utils/utils.js
@@ -2,4 +2,20 @@ function sum(num1, num2) {
   return num1 + num2;
 }
 
-module.exports = sum;
+function checkTypeSum(num1, num2) {
+  if (typeof num1 !== "number") {
+    throw new Error("invalid num1");
+  }
+
+  if (typeof num2 !== "number") {
+    throw new Error("invalid num2");
+  }
+}
+
+function fetchData() {
+  return {
+    name:'abc'
+  }
+}
+
+export { sum,checkTypeSum,fetchData };
